0a9c292249f704b893afdb0de41e309658ca55a0
shafiulislam1122 <98739731+shafiulislam1122@users.noreply.github.com>
2022-01-31 17:08:17 +0600

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,56 @@
+<html>
+<body>
+    <h2>HTML Forms</h2>
+
+    <from action="/action_page.php">
+        First Name :
+        <input type="text"first name= "first name" placeholder>
+        <br>
+        <table>
+            <tr>
+            <td>last name: </td>
+            <td>
+        <input type="text" last name= "last name" placeholder= >
+</td>
+</tr>
+
+
+            <tr>
+            <td>Age: </td>
+            <td> 
+        <input type="text" age = "age" placeholder>
+        </td>
+</tr>
+        
+<tr>
+            <td> Designation : </td>
+            <td>
+               
+        <input type ="radio" name="designation ">junior programmer 
+        <input type ="radio" name="designation ">senior programmer 
+        <input type ="radio" name="designation ">project lead
+        </td>
+</tr>
+
+
+
+
+<tr>
+            <td> Email : </td>
+            <td>
+
+        <input type="text"email= "email" placeholder>
+        </td>
+</tr>
+      
+        <tr>
+            <td>   Password : </td>
+            <td>
+        <input type="text"password= "password" placeholder>
+        
+        </td>
+</tr>
+
+</form>
+</body>
+</html>
